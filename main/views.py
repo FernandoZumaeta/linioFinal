60eba4c15828d18cf402b4df6bb2efaf4b99d1d5
FernandoZumaeta <74439823+FernandoZumaeta@users.noreply.github.com>
2020-11-30 10:47:39 -0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -268,7 +268,7 @@ class PedidosClienteDetailView(DetailView):
   model = Pedido
   template_name = "main/pedidos_cliente_detail.html"
 
-  def get_context_datat(self, **kwargs):
+  def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
     context['productos_cli'] = context['object'].detallepedido_set.all()
     return context
